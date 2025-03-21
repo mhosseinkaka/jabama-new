0200ad9acfbea725ac1f537d407fee9e03ce5234
mhosseinkaka <mhosseinkaka@gmail.com>
2025-03-21 15:09:59 +0330

    update colbe

--- colbe/admin.py
+++ colbe/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from colbe.models import Owner, User, Place
 # Register your models here.
+admin.site.register(Owner)
+admin.site.register(User)
+admin.site.register(Place)
--- colbe/urls.py
+++ colbe/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from colbe.views import cottage
 
 urlpatterns = [
+    path('', cottage)
 ]
