5495e6d4d029ff528f0803aca8e0bec3f5107d70
mhosseinkaka <mhosseinkaka@gmail.com>
2025-04-11 11:24:39 +0330

    aparteman update

--- aparteman/views.py
+++ aparteman/views.py
@@ -1,64 +1,22 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse, JsonResponse
-from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 from aparteman.models import Place
-from aparteman.serializers import ApartemanListSerializer
+from aparteman.serializers import ApartemanListSerializer, ApartemanSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 # Create your views here.     
 
-def aparteman_view_royayee(request):
-    data = [
-        {
-            "name":"آپارتمان در قیطریه",
-            "price":1000000
-        },
-        {
-            "name":"آپارتمان در قیطریه",
-            "price":1000000
-        },
-        {
-            "name":"آپارتمان در قیطریه",
-            "price":1000000
-        }
-    ]
-    return render(request,"aparteman.html",
-        context={
-                "type":"با ویو رویایی",
-                "data":data
-                })
 
 class ApartemanList(ListAPIView):
     queryset = Place.objects.all()
     serializer_class = ApartemanListSerializer
 
-class ApartemanLCView(ListCreateAPIView):
+class ApartemanCreateView(CreateAPIView):
+    permission_classes = [IsAdminUser]
     queryset = Place.objects.all()
-    serializer_class = ApartemanListSerializer
+    serializer_class = ApartemanSerializer
 
 class ApartemanRUDview(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Place.objects.all()
-    serializer_class = ApartemanListSerializer
-
-def aparteman_sargarmi(request):
-    data = [
-        {
-            "name":"آپارتمان در نیاوران",
-            "price":40000000
-        },
-        {
-            "name":"آپارتمان در نیاوران",
-            "price":40000000
-        },
-        {
-            "name":"آپارتمان در نیاوران",
-            "price":40000000
-        },
-        {
-            "name":"آپارتمان در نیاوران",
-            "price":40000000
-        },
-    ]
-    return render(request,"aparteman.html",
-        context={
-                "type":"با سرگرمی",
-                "data":data
-                })
+    serializer_class = ApartemanSerializer
+    
